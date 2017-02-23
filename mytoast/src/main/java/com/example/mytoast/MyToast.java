b23217ed5c276725c33b88ef8371bedc1a4c60b1
ChenTian <chentian@ChenTiandeMacBook-Pro.local>
2017-02-23 16:10:59 +0800

    Update NormalToast

--- mytoast/src/main/java/com/example/mytoast/MyToast.java
+++ mytoast/src/main/java/com/example/mytoast/MyToast.java
@@ -2,8 +2,6 @@ package com.example.mytoast;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
-import android.util.DisplayMetrics;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
@@ -29,7 +27,7 @@ public class MyToast {
      * @param toastStyle
      * @return
      */
-    public static Toast makeMyToast(Context context,String text,int image,int toastStyle){
+    public static Toast makeMyToast(Context context,String text,int image,int toastStyle,int duration){
         //加载Toast布局
         View toastRoot = LayoutInflater.from(context).inflate(R.layout.toast_item, null);
         //初始化布局控件
@@ -57,35 +55,34 @@ public class MyToast {
 
         //Toast的初始化
         Toast toast = new Toast(context);
-        //获取屏幕高度
-        DisplayMetrics dm = context.getResources().getDisplayMetrics();
-        int height = dm.heightPixels;
-        //Toast的Y坐标是屏幕高度的1/3，不会出现不适配的问题
-        toast.setGravity(Gravity.TOP, 0, height*3 / 4);
-        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setDuration(duration);
         toast.setView(toastRoot);
 
         return toast;
     }
 
     //以下分别是内置的错误，完成，点赞，保存，删除的Toast
-    public static void errorToast(Context context){
-        MyToast.makeMyToast(context,"ERROR!", R.drawable.error_icon, R.drawable.error_style).show();
+    public static Toast errorToast(Context context,String text,int duration){
+        return MyToast.makeMyToast(context,text,R.drawable.error_icon,R.drawable.error_style,duration);
     }
 
-    public static void finishToast(Context context){
-        MyToast.makeMyToast(context,"FINISH!", R.drawable.finish_icon, R.drawable.finish_style).show();
+    public static Toast finishToast(Context context,String text,int duration){
+        return MyToast.makeMyToast(context,text,R.drawable.finish_icon,R.drawable.finish_style,duration);
     }
 
-    public static void appreciateToast(Context context){
-        MyToast.makeMyToast(context,"APPRECIATE!", R.drawable.appreciate_icon, R.drawable.appreciate_style).show();
+    public static Toast appreciateToast(Context context,String text,int duration){
+        return MyToast.makeMyToast(context,text,R.drawable.appreciate_icon,R.drawable.appreciate_style,duration);
     }
 
-    public static void savedToast(Context context){
-        MyToast.makeMyToast(context,"SAVED!", R.drawable.saved_icon, R.drawable.saved_style).show();
+    public static Toast savedToast(Context context,String text,int duration){
+        return MyToast.makeMyToast(context,text,R.drawable.saved_icon,R.drawable.saved_style,duration);
     }
 
-    public static void deleteToast(Context context){
-        MyToast.makeMyToast(context,"DELETE!", R.drawable.delete_icon, R.drawable.delete_style).show();
+    public static Toast deleteToast(Context context,String text,int duration){
+        return MyToast.makeMyToast(context,text,R.drawable.delete_icon,R.drawable.delete_style,duration);
+    }
+
+    public static Toast NormalToast(Context context,String text,int duration){
+        return Toast.makeText(context,text,duration);
     }
 }
